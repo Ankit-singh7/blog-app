0f70b19dae8c572dbf9b6080dc4916f84ef392ee
Ankit-singh72 <Ankit-singh7>
2019-01-01 15:04:45 +0530

    first commit

--- src/app/blog-http.service.ts
+++ src/app/blog-http.service.ts
@@ -11,25 +11,26 @@ import { Observable } from "rxjs/Observable";
 //import { catchError,map} from rxjs/operators;
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/do';
+//import 'rxjs/add/operator/throw';
 
 @Injectable()
 export class BlogHttpService {
   public currentBlog;
   public allBlogs;
   public baseUrl = 'https://blogapp.edwisor.com/api/v1/blogs';
-  public AccessToken = 'MzJhN2M0NjAwOWM1ZGQzZDgyMWVhYjgwYTk1MDIyZTI4ODRlZmZjOTlmOWYwNTMxMGNhMTgwN2QxYzU3Y2IwZGU3OTUwOGU3MzNhMTAwYWY0YTZjNzYzNWI1OTViNTVmYTU0ZjFjZWUxYjIxOTMyMjFjZWJlMTJlNmNkMDNjYWJiZA==';
+  public AccessToken = 'ZWFkNTM3NWVhOWFhMTVmOGU4ODMxZTJhNjc2N2Q5ZTIwNWMwZWYxMDEzMWYzOTcwNTdiODNiYTBlNGE2ODk5OTdhMjhjZmNiNGNmMzY4YWMyMDAwMTE2YTA5OTNjMGEyZGZhODRmMmRjZjc4YzY3ZDg1OWU5OGUyNWIxYTE3ZDA4MA==';
   constructor(private _http: HttpClient) { }
 
   private handleError(err: HttpErrorResponse) {
     console.log("Handle error Http calls");
     console.log("err.message");
-    return Observable.throw(err.message)
+    return Observable.throw(err.message||"Server Error")
   }
 
   //method to return all the blogs
   public getAllBlogs(): any {
 
-    let myResponse = this._http.get(this.baseUrl + '/all?authToken=' + this.AccessToken);
+    let myResponse = this._http.get(this.baseUrl + '/all?authToken=' + this.AccessToken).catch(this.handleError);
     console.log(myResponse);
     return myResponse;
 
@@ -40,7 +41,7 @@ export class BlogHttpService {
   //method to get a particular blog 
   public getSingleBlogInformation(currentBlogId): any {
 
-    let myResponse = this._http.get(this.baseUrl + '/view' + '/' + currentBlogId + '?authToken=' + this.AccessToken);
+    let myResponse = this._http.get(this.baseUrl + '/view' + '/' + currentBlogId + '?authToken=' + this.AccessToken).catch(this.handleError);
     return myResponse;
 
 
@@ -48,20 +49,20 @@ export class BlogHttpService {
 
   public createBlog(blogData)
   {
-    let myResponse=this._http.post(this.baseUrl+'/create'+'?authToken='+this.AccessToken,blogData);
+    let myResponse=this._http.post(this.baseUrl+'/create'+'?authToken='+this.AccessToken,blogData).catch(this.handleError);
     return myResponse;
   }
    
   public deleteBlog(blogId)
   {
     let data={}
-    let myResponse=this._http.post(this.baseUrl+'/'+blogId+'/delete'+'?authToken='+this.AccessToken,data);
+    let myResponse=this._http.post(this.baseUrl+'/'+blogId+'/delete'+'?authToken='+this.AccessToken,data).catch(this.handleError);
     return myResponse;
   }
 
   public editBlog(blogId,blogData)
   {
-    let myResponse=this._http.put(this.baseUrl+'/'+blogId+'/edit'+'?authToken='+this.AccessToken,blogData);
+    let myResponse=this._http.put(this.baseUrl+'/'+blogId+'/edit'+'?authToken='+this.AccessToken,blogData).catch(this.handleError);
     return myResponse;
   }
 
